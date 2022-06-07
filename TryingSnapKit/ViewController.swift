3de1ccaec858a5a8a83316e0639ae99e96780d1f
Alexey Gulyaev <ghost-shadows@yandex.ru>
2022-06-07 11:24:22 +0300

    Trying use SnapKit in two easy view

--- TryingSnapKit/ViewController.swift
+++ TryingSnapKit/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    private lazy var myView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        return view
+    }()
+    
+    private lazy var bottomView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .green
+        view.layer.cornerRadius = 8
+        return view
+    }()
 
+    private func setupUI() {
+        view.addSubview(myView)
+        myView.addSubview(bottomView)
+        
+        myView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(100)
+            make.top.bottom.equalToSuperview().inset(200)
+        }
+        
+        bottomView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.top.bottom.equalToSuperview().inset(30)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
-
-
 }
-
