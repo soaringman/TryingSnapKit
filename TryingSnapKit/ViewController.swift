a81e4f54df204d85e08d4d3e8493ed68802f51fd
Alexey Gulyaev <ghost-shadows@yandex.ru>
2022-06-07 10:45:58 +0300

    Initial Commit

---
+++ TryingSnapKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TryingSnapKit
+//
+//  Created by Алексей Гуляев on 07.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
